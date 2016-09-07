f29f4b8937f08fb29a4a46eefdd552eeb7a65246
Gregorio <gregorio.freidin@gmail.com>
2016-09-07 13:34:41 -0300

    subo todo

---
+++ filtros/rotar_canales_asm.asm
@@ -0,0 +1,14 @@
+section .data
+DEFAULT REL
+
+
+
+section .text
+global rotar_asm
+;unsigned char *src, unsigned char *dst, int cols, int filas, int src_row_size, int dst_row_size
+rotar_asm:
+; rdi = putnero a matriz src
+; rsi = puntero a matriz dst
+; edx = tamaño de columnas
+; NOSE COMO SEGUIR ESTO :DDDDDDDDDDDDDDD		
+	ret
