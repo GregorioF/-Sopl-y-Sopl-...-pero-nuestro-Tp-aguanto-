9d996f0215242ffdd24d78c2fe0b5d97acfe3a8f
Gregorio <gregorio.freidin@gmail.com>
2016-09-08 23:57:51 -0300

    ta rotar en C y asm

--- filtros/rotar_canales_asm.asm
+++ filtros/rotar_canales_asm.asm
@@ -16,38 +16,23 @@ rotar_asm:
 	mul ecx			; en rax tengo la  cantidad d pilxeles totales
 	
 	.HacerMascaras:
-		mov r8, 0x0000ff000000ff00
+		pxor xmm4, xmm4
+		mov r8, 0x0f0c0e0d0b080a09 	; hago la mascara para shuffle
+		
 		movq xmm3, r8
 		pslldq xmm3, 8
-		movq xmm3, r8		; LO QUE HAGO ACA ES CREAR UNA MASCARA EN XMM3, TAL QUE SI HAGO AND CON XMM0 ME QDE LOS BYTES DE BLUE Y GREEN NADA MAS
+		mov r8, 0x0704060503000201
+		movq xmm4, r8
+		pxor xmm3, xmm4	 	; termine de hacer la mascara para shufle si el pixel x era = a|b|g|r ahora con shufle
+							; va a ser igual a  a|g|r|b
 		
-		mov r9, 0xffff0000ffff0000
-		movq xmm4, r9
-		pslldq xmm4, 8
-		movq xmm4, r9		; lo mismo pero ahora me qdarian red y transaprecia
-				
 	.ciclo:
 		cmp rax,  0 
 		je .fin
 		movdqu xmm0, [rdi]	 ; subo a xmm0 4  pixeles
-		movdqu xmm1, xmm0	; copio lo que levante en xmm0
-		movdqu xmm2, xmm0	; copio lo que levante en xmm0 
-		pslldq xmm0, 1		; corro todo un bbyte a la izquierda ya puse olos valores correspondientes en blue y green falta el del red
-		pand xmm0, xmm4		; en xmm0, las posiciones donde van red y transp me qdan en 0
-		
-		psrldq xmm1, 2		; ahora el blue me qda en la posicion del red
-		pand xmm1, xmm3		; ahora me qdan todos los bits en cero menos los del valor del red
-		
-		paddsb xmm0, xmm1	; ahora me qda con repecto a los valores iniciales  g|r|b|0 , me flata setear el a
-		
-		
-		pslldq xmm2, 1		; ahjora la transparencia me qda en la posicion del red
-		pan xmm2, xmm3		; ahora me qda todos los bytes en cero menos los del valor de red, donde est ala transparencia
-		psrldq xmm2, 1		; vuelvo la transparencia a su lugar inicial
-		
-		paddsb xmm0, xmm2	; ahora me qda con respecto a los valores iniciales g|r|b|a y listo
-		
-		movdqu [rsi], xmm0
+		pshufb xmm0, xmm3	; shufleee!
+		;psrldq xmm0, 1
+		movdqu [rsi], xmm0	; reescribo y lesto
 		
 		sub rax, 4			; procese ya cuatro pixeles
 		add rdi, 16
