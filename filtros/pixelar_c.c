a9ac540b08824c690ca581314fe20176644002f3
Gregorio <gregorio.freidin@gmail.com>
2016-09-09 22:08:09 -0300

    pixelar C ya esta

--- filtros/pixelar_c.c
+++ filtros/pixelar_c.c
@@ -1,17 +1,22 @@
 
 #include "../tp2.h"
 
-unsigned char promedio( unsigned char a , unsigned char b , unsigned char c , unsigned char d ){
-	return (a+b+c+d)/4;
+float promedio( unsigned char a , unsigned char b , unsigned char c , unsigned char d ){
+	float af = a; 
+	float bf = b;
+	float cf = c;
+	float df = d;
+	return (af/4+bf/4+cf/4+df/4);
+
 }
 
-void rotar_c (unsigned char *src, unsigned char *dst, int cols, int filas, int src_row_size, int dst_row_size) {
+void pixelar_c (unsigned char *src, unsigned char *dst, int cols, int filas, int src_row_size, int dst_row_size) {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
 	//COMPLETAR
-	for (int f = 0; f < filas; f++) {
-		for (int c = 0; c < cols; c++) {
+	for (int f = 0; f < filas; f+=2) {
+		for (int c = 0; c < cols; c+=2) {
 			bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
             bgra_t *p_s = (bgra_t*) &src_matrix[f][c * 4];
 
@@ -24,10 +29,12 @@ void rotar_c (unsigned char *src, unsigned char *dst, int cols, int filas, int s
 			bgra_t *p_d4 = (bgra_t*) &dst_matrix[f+1][c * 4];
             bgra_t *p_s4 = (bgra_t*) &src_matrix[f+1][c * 4];
 
-			unsigned char b = promedio ( p_s->b, p_s2->b, p_s3->b, p_s4->b );
-			unsigned char g = promedio ( p_s->g, p_s2->g, p_s3->g, p_s4->g );
-			unsigned char r = promedio ( p_s->r, p_s2->r, p_s3->r, p_s4->r );
-			unsigned char a = promedio ( p_s->a, p_s2->a, p_s3->a, p_s4->a );
+            float k = 0.5;
+
+			unsigned char b = promedio ( p_s->b, p_s2->b, p_s3->b, p_s4->b ) ;
+			unsigned char g = promedio ( p_s->g, p_s2->g, p_s3->g, p_s4->g ) ;
+			unsigned char r = promedio ( p_s->r, p_s2->r, p_s3->r, p_s4->r ) ;
+			unsigned char a = promedio ( p_s->a, p_s2->a, p_s3->a, p_s4->a ) ;
 
 
 			p_d->b = b;
