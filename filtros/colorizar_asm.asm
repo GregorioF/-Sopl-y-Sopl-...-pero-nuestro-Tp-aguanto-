3cfaf5600351b6f8de29eaa4b6cfe4c200cfd214
luuliirome <luciainesromer@hotmail.com>
2016-09-18 18:22:44 -0300

    a ver si anda

--- filtros/colorizar_asm.asm
+++ filtros/colorizar_asm.asm
@@ -28,41 +28,46 @@ section .text
 
 colorizar_asm:
 
-;alpha;
-
-	mov r10, 1
-	movq xmm7, r10
-	addps xmm8, xmm7 ; guardo 1+alpha en xmm8
-	subps xmm7, xmm9 ; guardo 1-alpha en xmm7
+movups xmm6, xmm0
+xor rax, rax
+inc rax
+mov r12, rdx
+mul r12
+mul rcx
+mov rdx, r12
+xor r12,r12
 
 .HacerMascaras: 
 		pxor xmm4, xmm4	;								a|r|g|b
 		
-		;creo mascaras en xmm5 y xmm6 para q me queden : a|b|g|b y a|g|r|r
+		;creo mascaras en xmm5 me quede : b|b|b|b
 
-		mov r10, 0x0f0c0d0c0b080908 	; hago la mascara para shuffle
-		
+		mov r10, 0x0f0e0d0c0b0a0908 	; hago la mascara para shuffle
+
 		movq xmm5, r10
-		pslldq xmm5, 8
-		mov r10, 0x0704050403000100
-		movq xmm4, r10
-		por xmm5, xmm4
 
-		mov r10, 0x0f0d0e0e0b090a0a
+		pslldq xmm5, 8 ;shifteo a izquierda 8 bytes
 
-		movq xmm6, r10
-		pslldq xmm6, 8
-		mov r10, 0x0705060603010202
+		mov r10, 0x0404040400000000
+
 		movq xmm4, r10
-		por xmm6, xmm4
+
+		por xmm5,xmm4 ; tengo la mascara en xmm5
+		
+
 
 
 .ciclo:
-	movdqu xmm0, [mascaraPrueba] 
-	call pasarDeMenosUnoAUnoYDeCeroAMenosUno
+
+	inc r12
+	inc r12
+
+	;movdqu xmm0, [mascaraPrueba] 
+	;call pasarDeMenosUnoAUnoYDeCeroAMenosUno
 	
 	movups xmm1, [rdi + rdx + rdx]; en xmm1 guardo los 3 pixeles de arriba
-	movups xmm2, [rdi + rdx]; en xmm2 guardo los 3 pixeles ppales (el pixel 1 y el 2 son los q estoy calculando)
+	movups xmm7, [rdi + rdx]; en xmm2 guardo los 3 pixeles ppales (el pixel 1 y el 2 son los q estoy calculando)
+	movups xmm2, xmm7
 	movups xmm3, [rdi]; en xmm3 guardo los 3 pixeles de abajo
 
 	pmaxsb xmm1, xmm2 ; guardo el maximo de cada byte en xmm1
@@ -90,14 +95,79 @@ colorizar_asm:
 
 	movups xmm1, xmm2
 
+	psrldq xmm1, 1 ; shifteo 1 byte, me queda 0|a|max(r)|max(g)|max(b)|a|max(r)|max(g)
 
-	
+	pmaxsb xmm1,xmm2 ; me quedan a|max(a,r)|max(r,g)|max(g,b)|max(b,a)|max(a,r)|max(r,g)|max(g,b)
+
+	movups xmm2,xmm1
+
+	psrldq xmm1,1 ;  shifteo 1 byte, me queda 0|a|max(a,r)|max(r,g)|max(g,b)|max(b,a)|max(a,r)|max(r,g)
+
+	pmaxsb xmm1, xmm2 ; me quedan a|max1(a,r)|max1(a,r,g)|max1(r,g,b)|max0(a,g,b)|max0(a,r,b)|max0(a,r,g)|max0(r,g,b)
+
+	pshufb xmm1, xmm5 ; en xmm1 me quedan basura|max1(rgb)|max1(rgb)|max1(rgb)|max1(rgb)||max0(rgb)|max0(rgb)|max0(rgb)|max0(rgb)
+
+	movups xmm2, xmm7
+
+	psrldq xmm2, 4 ; shifteo 4 bytes a la derecha
+
+	pcmpeqb xmm1, xmm2 ; comparo el mayor de todos con a|r|g|b original
+
+	; me queda en xmm1 basura| ceros o -1s
+
+	punpcklbw xmm1, xmm1 ; paso de bytes a words
+
+	movups xmm2, xmm1
+
+	punpcklwd xmm2, xmm1 ; paso de words a doble words el pixel de la pte baja
+
+	punpckhwd xmm1, xmm1 ; paso de words a double words el pixel de la pte alta
+
+	movups xmm0, xmm1
+
+	call pasarDeMenosUnoAUnoYDeCeroAMenosUno
+
+	movups xmm1, xmm0
+
+	movups xmm0, xmm2
+
+	call pasarDeMenosUnoAUnoYDeCeroAMenosUno
+
+	addps xmm1, xmm6
+
+	addps xmm2, xmm6
+
+	pxor xmm0, xmm0
+
+	punpcklwd xmm7, xmm0
+
+	movups xmm8, xmm7
+
+	punpckhwd xmm8, xmm0
+	punpcklwd xmm7, xmm0
+
+	mulps xmm7, xmm2
+	mulps xmm8, xmm1
+
+	packssdw xmm8, xmm7 ; quyedan pixel1|pixel0
+
+	packsswb xmm8, xmm8 ; quedan pixel1|pixel0|pixel1|pixel0
+
+	movq [rdi + 4], xmm8
+
+	lea rdi, [rdi + 16]
+
+	cmp r12, rax
+
+	jne .ciclo
 
 
 	ret
 	
 	
 ; pasarle un registro con floats sino no funca naAaaaaa!
+
+
 pasarDeMenosUnoAUnoYDeCeroAMenosUno:
 ; en xmm0 me viene el registro a cambiar de a dobles words son
 
