15c07ed7190fcfcb0685336485b9beb9f11b3f17
luuliirome <luciainesromer@hotmail.com>
2016-09-18 20:36:08 -0300

    subo mi smalltiles fijense si anda

--- filtros/smalltiles_asm.asm
+++ filtros/smalltiles_asm.asm
@@ -1,95 +1,61 @@
-%define i [rbp-8]
-%define j [rbp-16]
-
 section .data
 DEFAULT REL
 
 section .text
 global smalltiles_asm
-; void smalltiles_c (unsigned char *src, unsigned char *dst, int cols, int filas, int src_row_size, int dst_row_size) {
+
 smalltiles_asm:
-; rdi = src
-; rsi = dst
-; rdx = cols
-; rcx = filas
 	push rbp
 	mov rbp, rsp
-	sub rsp, 8
-	sub rsp, 8
+	push rbx
 	push r12
 
-	mov rax, rdx   
-	shr rax,1
-	mov i, rax 		; en i tengo la mitad de columnas que hay
+	mov rbx, rdx ; rbx = cantidad de bytes x fila
+	mov r12, rcx ; r12 = cant de filas
 
-	mov rax, rdx
-	shl rax, 2 		; rax = rdx*4 = tamño de fila
+	shr r12,1 ;psrlq r12, 1
+	shr rbx,1 ;psrlq rbx, 1
 
-	mov r12, rcx	; en r12 tengo la cantidad de filas que hay
-	shr r12, 1		; ahora la mitad
-	mov qword j, 0
-	
-		
-	.opAux:
-		add j, rax	; por cada fila le sumo a j 
-		sub r12, 1
-		cmp r12, 0
-		jne .opAux
+	xor r8, r8 ; contador
+	;rax = cant de bytes de una de las 4 imagenes destino
+	mov rax, r12
+	mul rdx
 
-	mov r12, j			; ahora r12 = #cols*4*#filas/2 (para saltar media imagen)
-
-	xor r9, r9 												; va a ser mi currrent de filas
+	;mascara:
+	mov r10, 0x0b0a090803020100
+	movq xmm2, r10
+	pslldq xmm2, 8
+	movq xmm3, r10
+	por xmm2, xmm3 ;|me queda: pixel3|pixel1|pixel3|pixel1 xq solo quiero los pares
 
 	.ciclo:
-			xor r8, r8 											; va a ser mi curren de cols
-			xor r11, r11										; va a ser mi current sobre src
-			
-			cmp rcx, r9
-			jle .fin 											; si termine de recorrer todas mis filas me voy
-			.ciclo2:
-				movdqu xmm1, [rdi+r8*4] 			; xmm1 = p4|p3|p2|p1
-				movdqu xmm2, xmm1					; xmm2 = xmm1
-				pshufd xmm1, xmm2,  0xd8 			; xmm1 = p4|p2|p3|p1      los dos pixeles que me importan ahora estan en la parte alta
-				movdqu xmm10, [rdi+r8*4+16]	 		; xmm10 = p8|p7|p6|p5   le meto a mi xmm10 cuatro pixeles siguiente a los de xmm1
-				movdqu xmm2, xmm10					; xmm2 = xmm10
-				pshufd xmm10, xmm2, 0x2d			; xmm10 = p5|p7|p8|p6   los dos pixeles que me importan ahora estan en la parte baja de xmm10
 
-				psrldq xmm1, 8						; xmm1 = 0|0|p4|p2
-				pslldq xmm10, 8						; xmm10 = p8|p6|0|0
+		add r8, 16 							; voy a procesar 4 pixeles, x ende 16 bytes
+		movups xmm0, [rdi] 					; guardo en xmm0 los 4 pixeles de src (3,2,1,0)
+		lea rdi, [rdi + 16] 				; ahora apunto a los siguiente 4
+		movups xmm1, [rdi] 					; guardo en xmm1 los siguientes 4 pixeles de src (7,6,5,4)
+		pshufb xmm0, xmm2 					; xmm0 = |pixel2|pixel0|pixel2|pixel0
+		pshufb xmm1, xmm2 					; xmm1 = |pixel6|pixel4|pixel6|pixel4
+		psrldq xmm0, 8 						; xmm0 = 0|0|pixel2|pixel0
+		psrldq xmm1, 8 						; xmm1 = 0|0|pixel6|pixel4
+		pslldq xmm1, 8 						; xmm1 = pixel6|pixel4|0|0 
+		por xmm1, xmm0 						; me queda |xmm1|xmm0 osea |pixel6|pixel4|pixel2|pixel0
 
-				paddb xmm10, xmm1 					; xmm10 = p8|p6|p4|p2		
-				
-				movdqu [rsi+r11*4], xmm10
-				mov r10, r11
-				add r10, i							; i acordarse que es igual a la mitad d las coulmnas 
-				movdqu [rsi+r10*4], xmm10			; escribo en  la segunda imagen
+		movdqu [rsi], xmm0
+		movdqu [rsi + rbx], xmm0 			; sumo la cant de bytes hasta el final de la columna
+		mov r9, rax
+		add r9, rax
+		movdqu [rsi + r9], xmm0 			; sumo la cant de bytes de 2 mini fotos
+		add r9, r9 
+		movdqu [rsi + r9], xmm0 			; sumo la cant de bytes de 3 mini fotos 
 
+		lea rdi, [rdi + 16] 				; procese 4 pixeles desde q sume
+		lea rsi, [rsi + 16] 				; procese 4 pixeles
 
-				lea r10 , [r12+r11*4]				; para escribir en la tercer imagen
+		cmp r8, rax
+		jne .ciclo
 
-				movdqu [rsi+r10], xmm10				; lesto
-				
-				mov r10, r11			
-				add r10, i
-				
-				lea r10, [r12+ r10*4]
-				movdqu [rsi +r10], xmm10			; escribo en la cuarta imagen
-
-				add r8, 8							; lo avanzo cuatro numeros
-				add r11, 4							; imprimi cuatro pixeles en cada imagen
-				cmp r8, rdx							; 
-				jne .ciclo2
-
-				add r9, 2
-				add rdi, rax
-				add rdi, rax
-				add rsi, rax
-				jmp .ciclo
-
-
-	.fin:
-		pop r12
-		add rsp, 8
-		add rsp,8
-		pop rbp
-		ret
+	pop r12
+	pop rbx
+	pop rbp
+	ret
