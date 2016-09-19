3d71e796b75b24dd2a6c63c20798f8d0648bcbc3
luuliirome <luciainesromer@hotmail.com>
2016-09-18 23:02:50 -0300

    oh malditos currents!

--- filtros/smalltiles_asm.asm
+++ filtros/smalltiles_asm.asm
@@ -13,46 +13,43 @@ smalltiles_asm:
 	mov rbx, rdx ; rbx = cantidad de bytes x fila
 	mov r12, rcx ; r12 = cant de filas
 
-	shr r12,1 ;psrlq r12, 1
-	shr rbx,1 ;psrlq rbx, 1
-
+	xor r10, r10
+	mov rax, rbx
+	mul r12
+	;rax = cant de bytes total
+	mov r10, rax ; r10 = cant de pixeles totales
+	;shr rax, 2
+	
 	xor r8, r8 ; contador
 	;rax = cant de bytes de una de las 4 imagenes destino
-	mov rax, r12
-	mul rdx
-
-	;mascara:
-	mov r10, 0x0b0a090803020100
-	movq xmm2, r10
-	pslldq xmm2, 8
-	movq xmm3, r10
-	por xmm2, xmm3 ;|me queda: pixel3|pixel1|pixel3|pixel1 xq solo quiero los pares
+	
+	shl rbx,1
 
 	.ciclo:
 
-		add r8, 16 							; voy a procesar 4 pixeles, x ende 16 bytes
+		add r8, 8 							; voy a procesar 8 pixeles
 		movups xmm0, [rdi] 					; guardo en xmm0 los 4 pixeles de src (3,2,1,0)
 		lea rdi, [rdi + 16] 				; ahora apunto a los siguiente 4
 		movups xmm1, [rdi] 					; guardo en xmm1 los siguientes 4 pixeles de src (7,6,5,4)
-		pshufb xmm0, xmm2 					; xmm0 = |pixel2|pixel0|pixel2|pixel0
-		pshufb xmm1, xmm2 					; xmm1 = |pixel6|pixel4|pixel6|pixel4
+		pshufd xmm0, xmm0 , 0xdd			; xmm0 = |pixel4|pixel2|pixel4|pixel2
+		pshufd xmm1, xmm1 , 0xdd			; xmm1 = |pixel8|pixel6|pixel8|pixel6
 		psrldq xmm0, 8 						; xmm0 = 0|0|pixel2|pixel0
-		psrldq xmm1, 8 						; xmm1 = 0|0|pixel6|pixel4
 		pslldq xmm1, 8 						; xmm1 = pixel6|pixel4|0|0 
-		por xmm1, xmm0 						; me queda |xmm1|xmm0 osea |pixel6|pixel4|pixel2|pixel0
+		paddb xmm0, xmm1 						; me queda |xmm1|xmm0 osea |pixel6|pixel4|pixel2|pixel0
 
-		movdqu [rsi], xmm0
-		movdqu [rsi + rbx], xmm0 			; sumo la cant de bytes hasta el final de la columna
+		movups [rsi], xmm0
+		movups [rsi + r9], xmm0 			; sumo la cant de bytes hasta el final de la columna
 		mov r9, rax
 		add r9, rax
-		movdqu [rsi + r9], xmm0 			; sumo la cant de bytes de 2 mini fotos
-		add r9, r9 
-		movdqu [rsi + r9], xmm0 			; sumo la cant de bytes de 3 mini fotos 
+		
+		movups [rsi + r9], xmm0 			; sumo la cant de bytes de 2 mini fotos 
+		;add r9, r9
+		;movups [rsi + r9], xmm0
 
 		lea rdi, [rdi + 16] 				; procese 4 pixeles desde q sume
 		lea rsi, [rsi + 16] 				; procese 4 pixeles
 
-		cmp r8, rax
+		cmp r8, r10
 		jne .ciclo
 
 	pop r12
