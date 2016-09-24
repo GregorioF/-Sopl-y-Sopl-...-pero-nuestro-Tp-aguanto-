e04c3cbc260498797736d06ea64fa118634e84e9
luuliirome <luciainesromer@hotmail.com>
2016-09-24 17:18:14 -0300

    subo cosas

--- Experimentos/ROTAR/graficoRotar400x800.py
+++ Experimentos/ROTAR/graficoRotar400x800.py
@@ -1,6 +1,6 @@
 import csv
 
-data_csv = "rotar.csv"
+data_csv = "rotarRompiendo400x800.csv"
 
 ts = [ [] for i in range(128)]
 
@@ -9,40 +9,40 @@ with open(data_csv) as csvfile:
   for row in reader:
     rot = int(row['rotar'])
     
-    ts[100].append(rot)
+    ts[30].append(rot)
 
-data2_csv = "rotarC.csv"
+data2_csv = "rotarRompiendo800x400.csv"
 
 ts2 = [ [] for i in range(128)]
 
 with open(data2_csv) as csvfile:
   reader = csv.DictReader(csvfile)
   for row in reader:
-    rot1 = int(row['rotar'])
+    romp = int(row['rompiendo'])
     
-    ts2[30].append(rot1)
+    ts2[30].append(romp)
 
-data3_csv = "rotarCO2.csv"
+data3_csv = "rotarSinRomper400x800.csv"
 ts3 = [ [] for i in range(128)]
 
     
 with open(data3_csv) as csvfile:
   reader = csv.DictReader(csvfile)
   for row in reader:
-    rot2 = int(row['rotar'])
+    o5 = int(row['sinR'])
     
-    ts3[30].append(rot2)
+    ts3[30].append(o5)
 
-data4_csv = "rotarCO5.csv"
+data4_csv = "rotarSinRomper800x400.csv"
 ts4 = [ [] for i in range(128)]
 
     
 with open(data4_csv) as csvfile:
   reader = csv.DictReader(csvfile)
   for row in reader:
-    rot5 = int(row['rotar'])
+    sinR8 = int(row['sinR8'])
     
-    ts4[30].append(rot5)
+    ts4[30].append(sinR8)
 
 
 import matplotlib.pyplot as plt
@@ -54,27 +54,27 @@ for i in range(128):
 	for t in ts[i]:
 		rotar = min(rotar, t)
 
-rotar2 = 1000000
+romper = 1000000
 for i in range(128):
 	for t in ts2[i]:
-		rotar2 = min(rotar2, t)
+		romper = min(romper, t)
 
-rotar3 = 1000000
+co5 = 1000000
 for i in range(128):
 	for t in ts3[i]:
-		rotar3 = min(rotar3, t)
+		co5 = min(co5, t)
 
-rotar4 = 1000000
+sinRomp = 1000000
 for i in range(128):
   for t in ts4[i]:
-    rotar4 = min(rotar4, t)
+    sinRomp = min(sinRomp, t)
 
 
 y = []
+y.append(co5)
 y.append(rotar)
-y.append(rotar2)
-y.append(rotar3)
-y.append(rotar4)
+y.append(sinRomp)
+y.append(romper)
 
 
 ind = np.arange(4) 
@@ -86,13 +86,13 @@ rects1 = ax.bar(ind, y, width, color='r')
 ax.set_ylabel('Ciclos')
 ax.set_title('Diferencias de Rotar ASM llenando de basura la cache')
 ax.set_xticks(ind + 0.4)
-ax.set_xticklabels(('C','', 'C O2', 'C O5'))
+ax.set_xticklabels(('400x800', '400x800 Rompiendo', '800x400', '800x400 Rompiendo'))
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 rects1[0].set_color('b')
 rects1[1].set_color('g')
 rects1[2].set_color('r')
 rects1[3].set_color('y')
-plt.ylim(990000,1000200)
+plt.ylim(270000,281000)
 
 plt.show()
 
