522bfcca9fd1fe47af93378c35a4543c305407c6
luuliirome <luciainesromer@hotmail.com>
2016-09-24 22:59:41 -0300

    para ssh

--- filtros/rotar_canales_asm.asm
+++ filtros/rotar_canales_asm.asm
@@ -14,29 +14,22 @@ rotar_asm:
 	xor rax, rax
 	mov eax, edx	; en eax tengo la cnatidad de columnas
 	mul ecx			; en rax tengo la  cantidad d pilxeles totales
-	
-	.HacerMascaras:
-		pxor xmm4, xmm4
-		mov r8, 0x0f0c0e0d0b080a09 	; hago la mascara para shuffle
+
+
+		HacerMascaras:
 		
-		movq xmm3, r8
-		pslldq xmm3, 8
 		mov r8, 0x0704060503000201
-		movq xmm4, r8
-		por xmm3, xmm4	 	; termine de hacer la mascara para shufle si el pixel x era = a|b|g|r ahora con shufle
-							; va a ser igual a  a|g|r|b
-		
+				
 	.ciclo:
 		cmp rax,  0 
 		je .fin
-		movdqu xmm0, [rdi]	 ; subo a xmm0 4  pixeles
-		pshufb xmm0, xmm3	; shufleee!
-		;psrldq xmm0, 1
-		movdqu [rsi], xmm0	; reescribo y lesto
+		mov r10, [rdi]	 ; subo a r10 2  pixeles
+		pshufb r10, r8	; shufleee!
+		mov [rsi], r10	; reescribo y lesto
 		
-		sub rax, 4			; procese ya cuatro pixeles
-		add rdi, 16
-		add rsi, 16
+		sub rax, 2			; procese ya cuatro pixeles
+		add rdi, 8
+		add rsi, 8
 		jmp .ciclo
 			
 	.fin:
