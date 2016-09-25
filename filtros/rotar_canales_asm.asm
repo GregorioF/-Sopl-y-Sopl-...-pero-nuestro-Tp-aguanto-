7c604533580a54557cb325515bd3c2f4bab7bce2
luuliirome <luciainesromer@hotmail.com>
2016-09-24 23:04:51 -0300

    subo modificacion de rotar para experimento

--- filtros/rotar_canales_asm.asm
+++ filtros/rotar_canales_asm.asm
@@ -15,17 +15,22 @@ rotar_asm:
 	mov eax, edx	; en eax tengo la cnatidad de columnas
 	mul ecx			; en rax tengo la  cantidad d pilxeles totales
 
-
-		HacerMascaras:
-		
-		mov r8, 0x0704060503000201
-				
 	.ciclo:
 		cmp rax,  0 
 		je .fin
 		mov r10, [rdi]	 ; subo a r10 2  pixeles
-		pshufb r10, r8	; shufleee!
-		mov [rsi], r10	; reescribo y lesto
+		mov r11, r10
+		pslld r10, 2 ; |g|r|0|0
+		psrld r10, 1 ; 0|g|r|0
+		movq r9, r11
+		pslld r9, 1 ; a|b|g|r --> b|g|r|0
+		psrld r9, 3 ; b|g|r|0 --> 0|0|0|b
+		psrld r11, 3 ; a|b|g|r --> 0|0|0|a
+		pslld r11, 3 ; 0|0|0|a --> a|0|0|0
+		paddb r11, r9 ; a|0|0|b
+		paddb r10, r11 ; a|g|r|b
+		
+		movq [rsi], r10	; reescribo y lesto
 		
 		sub rax, 2			; procese ya cuatro pixeles
 		add rdi, 8
