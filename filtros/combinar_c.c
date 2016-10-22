1789d1f3e1f4e5b48ea8c8d1941806dd537c0314
Lucia <luciainesromero@hotmail.com>
2016-10-22 15:35:54 -0300

    prueba exp

--- filtros/combinar_c.c
+++ filtros/combinar_c.c
@@ -1,5 +1,8 @@
 
 #include "../tp2.h"
+#include "../helper/tiempo.h"
+
+char* name = "tiemposLeer.csv";
 
 float clamp(float pixel)
 {
@@ -15,21 +18,39 @@ float combine(unsigned char a, unsigned char b, float alpha){
 }
 
 void combinar_c (unsigned char *src, unsigned char *dst, int cols, int filas, int src_row_size, int dst_row_size,float alpha) {
+
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
 	for(int f = 0; f<filas; f++){
 		for(int c = 0; c<cols; c++){
 
+			volatile unsigned long long start, end;
+			MEDIR_TIEMPO_START(start);
+			//lectura:
 			bgra_t *p_sa = (bgra_t*) &src_matrix[f][c * 4];
 			bgra_t *p_sb = (bgra_t*) &src_matrix[f][(cols - c -1) * 4];
 			bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
+			MEDIR_TIEMPO_STOP(end);
 
+			// precesamiento:
+			float n = clamp(combine(p_sa->b, p_sb->b, alpha));
+			float n2 = clamp(combine(p_sa->g, p_sb->g, alpha));
+			float n3 = clamp(combine(p_sa->r, p_sb->r, alpha));
+			float n4 = clamp(combine(p_sa->a, p_sb->a, alpha));
 
-			p_d->b = clamp(combine(p_sa->b, p_sb->b, alpha));
-			p_d->g = clamp(combine(p_sa->g, p_sb->g, alpha));
-			p_d->r = clamp(combine(p_sa->r, p_sb->r, alpha));
-			p_d->a = clamp(combine(p_sa->a, p_sb->a, alpha));
+			//escritura:
+
+			p_d->b = n;
+			p_d->g = n2;
+			p_d->r = n3;
+			p_d->a = n4;
+
+			volatile unsigned long long int cant_ciclos = end-start;
+			FILE *pFile = fopen( name, "a" );
+			fprintf(pFile,"%.3f\n", (float)cant_ciclos/(float)cant_iteraciones);
+			fclose( pFile );
+
 
 		}
 	}
