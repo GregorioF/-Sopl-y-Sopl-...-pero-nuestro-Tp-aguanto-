382168e5fdfdce0593405a6dc0ec3019862bf82f
Lucia <luciainesromero@hotmail.com>
2016-10-27 13:38:08 -0300

    ssh

--- filtros/combinar.c
+++ filtros/combinar.c
@@ -5,6 +5,7 @@
 
 #include "../tp2.h"
 
+/*
 char* nombre = "tiemposCombinar.csv";
 
 void imprimirArchivo(long long unsigned int n){
@@ -13,7 +14,7 @@ void imprimirArchivo(long long unsigned int n){
 	fclose(pFile);
 }
 
-
+*/
 
 void combinar_asm    (unsigned char *src, unsigned char *dst, int cols, int filas,
                       int src_row_size, int dst_row_size, float alpha);
