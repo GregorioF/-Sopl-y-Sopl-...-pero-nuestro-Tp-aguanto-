f4b50d92e067cbede975ee4ef22ffbff2a15aec1
Lucia <luciainesromero@hotmail.com>
2016-11-02 13:53:30 -0300

    ssh

--- filtros/rotar_canales_asm.asm
+++ filtros/rotar_canales_asm.asm
@@ -11,33 +11,84 @@ rotar_asm:
 ; rsi = puntero a matriz dst
 ; edx = cantidad de columnas
 ; ecx = cantidad de filas
+
+	push rbp
+	mov rbp, rsp ;a|b|g|r --> a|g|r|b
+	push rbx
+	push r12
+	push r13
+	push r14
+
 	xor rax, rax
 	mov eax, edx	; en eax tengo la cnatidad de columnas
 	mul ecx			; en rax tengo la  cantidad d pilxeles totales
-	
-	.HacerMascaras:
-		pxor xmm4, xmm4
-		mov r8, 0x0f0c0e0d0b080a09 	; hago la mascara para shuffle
-		
-		movq xmm3, r8
-		pslldq xmm3, 8
-		mov r8, 0x0704060503000201
-		movq xmm4, r8
-		por xmm3, xmm4	 	; termine de hacer la mascara para shufle si el pixel x era = a|b|g|r ahora con shufle
-							; va a ser igual a  a|g|r|b
-		
+
 	.ciclo:
+		
 		cmp rax,  0 
 		je .fin
-		movdqu xmm0, [rdi]	 ; subo a xmm0 4  pixeles
-		pshufb xmm0, xmm3	; shufleee!
-		;psrldq xmm0, 1
-		movdqu [rsi], xmm0	; reescribo y lesto
+
+		xor r8, r8
+		xor r9, r9
+		xor r10, r10
+		xor r11, r11
+		xor r12, r12
+		xor r13,r13
+		xor r14,r14
+
+		mov r12, [rdi]	 ; subo a r10 2 pixeles
 		
-		sub rax, 4			; procese ya cuatro pixeles
-		add rdi, 16
-		add rsi, 16
+		mov r11b, [r12] ; subo a r11b un byte
+		shr r12, 8
+		mov r10b, [r12]
+		shr r12, 8
+		mov r9b, [r12]
+		shr r12, 8
+		mov r8b, [r12]
+		shr r12, 8
+		
+		mov r13, r10b
+		shl r13, 8
+		or r13, r8b
+		shl r13, 8
+		or r13, r9b
+		shl r13, 8
+		or r13, r11b
+		
+		mov r11b, [r12] ; subo a r11b un byte
+		shr r12, 8
+		mov r10b, [r12]
+		shr r12, 8
+		mov r9b, [r12]
+		shr r12, 8
+		mov r8b, [r12]
+		
+		mov r14, r10b
+		shl r14, 8
+		or r14, r8b
+		shl r14, 8
+		or r14, r9b
+		shl r14, 8
+		or r14, r11b
+		
+		shl r14, 32
+		or r14, r13
+
+		mov [rsi], r14
+		add rsi, 8
+		add rdi, 8
+
+
+		sub rax, 2			; procese dos pixeles
+				
 		jmp .ciclo
 			
 	.fin:
+
+		pop r14
+		pop r13
+		pop r12
+		pop rbx
+		pop rbp
+		
 		ret
